871f20d9be0174e97a298a2da74d79372103a87d
adwait2007 <74483768+adwait2007@users.noreply.github.com>
2020-12-28 11:15:52 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,52 @@
+var database,dog,happydog,dogIMG,foodS,foodStock;
+
+function preload(){
+  dogIMG=loadImage("images/dogImg.png");
+  happydog=loadImage("images/dogImg.png");
+}
+
+function setup(){
+  createCanvas(500,500);
+
+  database=firebase.database();
+
+  dog=createSprite(250,270,20,20);
+  dog.addImage("standing",dogIMG);
+  dog.scale=0.2;
+
+  foodStock=database.ref('Food');
+  foodStock.on("value",readStock);
+
+}
+
+function draw(){
+  background(46,139,73);
+
+  if(keyWentDown(UP_ARROW)){
+    writeStock(foodS);
+    dog.addImage("sitting",happydog);
+  }
+  drawSprites();
+
+  textSize(15);
+  fill("white");
+  text(" Note:Press UP_ARROW Key To Feed Drago Milk! ",100,50);
+
+}
+
+function readStock(data){
+   foodS=data.val();
+}
+
+function writeStock(x){
+
+  if(x>=0){
+    x=0;
+  }else{
+    x=x+1;
+  }
+
+  database.ref('/').update({
+    Food:x
+  });
+}
